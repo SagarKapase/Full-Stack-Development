d1608bc4f4a6e2d414bc894989b343a81f96c236
Sagar Kapase <97930178+SagarKapase@users.noreply.github.com>
2024-06-30 11:09:15 +0530

    Done

---
+++ Kafka/UserEnd-Service/UserEnd-Service/src/main/java/com/userend/UserEndServiceApplication.java
@@ -0,0 +1,13 @@
+package com.userend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserEndServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserEndServiceApplication.class, args);
+	}
+
+}
---
+++ Kafka/UserEnd-Service/UserEnd-Service/src/test/java/com/userend/UserEndServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.userend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserEndServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
